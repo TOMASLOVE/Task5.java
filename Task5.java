b76fea7d3b9ea27f58d7e2329387a920b89e9bce
TOMASLOVE <164284042+TOMASLOVE@users.noreply.github.com>
2024-04-29 03:37:31 +0000

    Done adding output

--- Task5.java
+++ Task5.java
@@ -1,19 +1,14 @@
-package mng.tvi.java.Task;
 
 import java.util.*;
 
-public class task{
-    public static void main (string[] args){
+public class Task5 {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("input a word");
         String inputWord = input.nextLine();
         StringBuilder sb = new StringBuilder(inputWord);
         String message = ((inputWord.equals(sb.reverse().toString())))? "The input string is a palindrome.": "The input string is not a palindrome.";
         System.out.println(message);
-        
-
-
     }
-
-
+    
 }
